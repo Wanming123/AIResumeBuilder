1a00e99a4765e0e6518387236a9e8a94b1060d22
Wanming123 <hwm020926@icloud.com>
2025-11-07 16:00:00 -0500

    frontend: Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,10 @@
-import { FilePenLineIcon, PlusIcon, UploadCloudIcon } from 'lucide-react'
+import { FilePenLineIcon, PencilIcon, PlusIcon, TrashIcon, UploadCloudIcon } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
 import { dummyResumeData } from '../assets/assets'
 
 const Dashboard = () => {
 
-  const colors = ['#9333EA', '#DB2777', '#0284C7', '#DC2626', '16a34a']
+  const colors = ['#9333EA', '#DB2777', '#0284C7', '#DC2626', '#16a34a']
 
   const [allResumes, setAllResumes] = useState([])
 
@@ -37,13 +37,17 @@ const Dashboard = () => {
           {allResumes.map((resume, index) => {
             const baseColor = colors[index % colors.length];
             return (
-              <button key={index} className='relative w-full sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 border group hover:shadow-lg transition-all duration-300 cursor-pointer' style={{background: 'linear-gradient(145deg, ${baseColor}10, ${baseColor}40)', borderColor: baseColor + '40'}}>
+              <button key={index} className='relative w-full sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 border group hover:shadow-lg transition-all duration-300 cursor-pointer' style={{background: `linear-gradient(145deg, ${baseColor}10, ${baseColor}40)`, borderColor: baseColor + '40'}}>
 
                 <FilePenLineIcon className='size-7 group-hover:scale-105 transition-all' style={{color: baseColor}}/>
                 <p className='text-sm group-hover:scale-105 transition-all px-2 text-center' style={{color: baseColor}}>{resume.title}</p>
                 <p className='absolute bottom-1 text-[11px] text-slate-400 group-hover:text-slate-500 transition-all duration-300 px-2 text-center' style={{color: baseColor + '90'}}>
                   Updated on {new Date(resume.updatedAt).toLocaleDateString()}
                 </p>
+                <div className='absolute top-1 right-1 group-hover:flex items-center hidden'>
+                  <TrashIcon className='size-7 p-1.5 text-slate-700 hover:bg-white/50 transition-colors rounded' />
+                  <PencilIcon className='size-7 p-1.5 text-slate-700 hover:bg-white/50 transition-colors rounded' />
+                </div>
 
               </button>
             )
